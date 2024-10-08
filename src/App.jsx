2a1cb030cf768880660cec03fef8e7502f14ae0f
akmalzidani <akmal20031003@gmail.com>
2024-10-08 21:42:48 +0700

    refactor: move data folder from /src to root folder

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
+import CityList from "./components/CityList";
 
 export default function App() {
   return (
@@ -15,7 +16,7 @@ export default function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<p>Dashboard</p>} />
+          <Route index element={<CityList />} />
           <Route path="cities" element={<p>List of cities</p>} />
           <Route path="countries" element={<p>Countries</p>} />
           <Route path="form" element={<p>Form</p>} />
