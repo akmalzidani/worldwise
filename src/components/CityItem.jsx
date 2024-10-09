0266aced2af6abed408e3e851ac162c9c36e9b52
akmalzidani <akmal20031003@gmail.com>
2024-10-09 15:40:31 +0700

    feat: add dynamic and nested routes

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,15 +1,18 @@
 import styles from "./CityItem.module.css";
 import { formatDate, formatEmoji } from "../utils/formatter";
+import { Link } from "react-router-dom";
 
 export default function CityItem({ city }) {
-  const { cityName, emoji, date } = city;
+  const { cityName, emoji, date, id } = city;
 
   return (
-    <li className={styles.cityItem}>
-      <span className={styles.emoji}>{formatEmoji(emoji)}</span>
-      <h3 className={styles.name}>{cityName}</h3>
-      <time className={styles.date}>{formatDate(date)}</time>
-      <button className={styles.deleteBtn}>&times;</button>
+    <li>
+      <Link className={styles.cityItem} to={`${id}`}>
+        <span className={styles.emoji}>{formatEmoji(emoji)}</span>
+        <h3 className={styles.name}>{cityName}</h3>
+        <time className={styles.date}>{formatDate(date)}</time>
+        <button className={styles.deleteBtn}>&times;</button>
+      </Link>
     </li>
   );
 }
