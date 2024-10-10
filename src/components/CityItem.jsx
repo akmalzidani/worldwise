e108d8522077d32a2ce5ce46292cab3c38f01d11
akmalzidani <akmal20031003@gmail.com>
2024-10-10 15:31:36 +0700

    refactor: refactor to useCities Context API feat : City Detail View style: currentCity active

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,8 +1,11 @@
 import styles from "./CityItem.module.css";
 import { formatDate, formatEmoji } from "../utils/formatter";
 import { Link } from "react-router-dom";
+import { useCities } from "../contexts/CitiesContext";
 
 export default function CityItem({ city }) {
+  const { currentCity } = useCities();
+
   const {
     cityName,
     emoji,
@@ -13,7 +16,12 @@ export default function CityItem({ city }) {
 
   return (
     <li>
-      <Link className={styles.cityItem} to={`${id}?lat=${lat}&lng=${lng}`}>
+      <Link
+        className={`${styles.cityItem} ${
+          id === currentCity.id ? styles["cityItem--active"] : ""
+        }`}
+        to={`${id}?lat=${lat}&lng=${lng}`}
+      >
         <span className={styles.emoji}>{formatEmoji(emoji)}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>{formatDate(date)}</time>
